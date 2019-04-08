3fbc52b3f26b8801ab1c4df365d11f7f24856680
Fasdr <43724226+Fasdr@users.noreply.github.com>
2019-04-08 16:03:30 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,20 +7,35 @@ def funca(s):
     n = copy.deepcopy(s)
     for j in range(1, N - 1):
         for i in range(1, j):
-            print(0)
+            n[j][i] = (1-w)*s[j][i]+(w/(2*a*b))*(a*n[j-1][i]+b*n[j][i-1]+a*s[j+1][i]+b*s[j][i+1]+(h**2)*F[j][i])
+    return n
+
+def prod(s1, s2):
+    prsum = 0
+    for j in range(N):
+        for i in range(j+1):
+            prsum += h*h * s1[j][i] * s2[j][i]
+    return prsum
+
+
+def norm(s):
+    return math.sqrt(prod(s, s))
+
+def mdif(s1, s2):
+    n = copy.deepcopy(s1)
+    for j in range(N):
+        for i in range(j+1):
+            n[j][i] -= s2[j][i]
     return n
 
 
 
-
-
-
-
-N = 5
+N = 41
 h = 1 / (N-1)
 a = 1
 b = 1.2
-d = 1 / 10 ** 6
+d = 1/10**6
+w = 1.1
 S = [[(np.sign(i * (j - i) * (N - j - 1))) for i in range(j + 1)] for j in range(N)]
 F = [[0.2*math.exp(h*i)*math.cos(h*j) for i in range(j + 1)] for j in range(N)]
 for j in range(0, N-1):
@@ -29,5 +44,15 @@ for j in range(0, N-1):
 for i in range(0, N):
     S[N-1][i] = math.exp(h * i) * math.cos(h * (N-1))
 
-print(S)
-print(F)
+print(norm(S))
+k = 1
+FM = copy.deepcopy(S)
+SM = funca(FM)
+while norm(mdif(SM,FM))>d:
+    k += 1
+    SM,FM = funca(SM),SM
+
+print(norm(SM))
+print(norm(FM))
+print(k) 
+print(norm(mdif(SM,FM)))
