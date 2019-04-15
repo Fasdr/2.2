3ced63a8f29f6666664dac2c58bde925e593a1c7
Fasdr <43724226+Fasdr@users.noreply.github.com>
2019-04-15 16:03:19 +0700

    Update main.py

--- main.py
+++ main.py
@@ -11,12 +11,24 @@ def funca(s):
     return n
 
 
+def ufunc(s):
+    n = copy.deepcopy(s)
+    for j in range(1, N - 1):
+        for i in range(1, j):
+            n[j][i] = -a*(s[j-1][i]-2*s[j][i]+s[j+1][i])-b*(s[j][i-1]-2*s[j][i]+s[j][i+1])
+    return n
+
+
 def prod(s1, s2):
     prsum = 0
     for j in range(N):
         for i in range(j+1):
             prsum += h*h * s1[j][i] * s2[j][i]
     return prsum
+
+
+def resid(s):
+    return mdif(ufunc(s),rF)
 
 
 def norm(s):
@@ -31,7 +43,18 @@ def mdif(s1, s2):
     return n
 
 
+def mults(k,s):
+    n = copy.deepcopy(s)
+    for j in range(N):
+        for i in range(j+1):
+            n[j][i] = k * s[j][i]
+    return n
 
+
+def tau(s):
+    return (prod(ufunc(s),s))/(prod(ufunc(s),ufunc(s)))
+    
+    
 N = 6
 h = 1 / (N-1)
 a = 1
@@ -40,6 +63,7 @@ d = 1/10**6
 w = 1.1
 S = [[(np.sign(i * (j - i) * (N - j - 1))) for i in range(j + 1)] for j in range(N)]
 F = [[0.2*math.exp(h*i)*math.cos(h*j) for i in range(j + 1)] for j in range(N)]
+rF = mults(h**2,F)
 Fi = [[math.exp(h*i)*math.cos(h*j) for i in range(j + 1)] for j in range(N)]
 for j in range(0, N-1):
     S[j][0] = math.cos(h*j)
@@ -47,12 +71,29 @@ for j in range(0, N-1):
 for i in range(0, N):
     S[N-1][i] = math.exp(h * i) * math.cos(h * (N-1))
 
-k = 1
-FM = copy.deepcopy(S)
-SM = funca(FM)
-while norm(mdif(SM, FM)) > d:
-    k += 1
-    SM, FM = funca(SM), SM
 
-print(k)
-print(norm(mdif(Fi,SM)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#k = 1
+#FM = copy.deepcopy(S)
+#SM = funca(FM)
+#while norm(mdif(SM, FM)) > d:
+#    k += 1
+#    SM, FM = funca(SM), SM
+#
+#print(k)
+#print(norm(mdif(Fi,SM)))
